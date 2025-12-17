59b65f10a4110553451e084bf0fdcf2d580f09d2
Eugene_pavshiy <keki4noob@gmail.com>
2025-12-17 13:20:16 +0100

    ...

--- team_compare.cpp
+++ team_compare.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   team_compare.cpp                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yyaniv <yyaniv@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/17 13:20:07 by yyaniv            #+#    #+#             */
+/*   Updated: 2025/12/17 13:20:07 by yyaniv           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -5,10 +17,6 @@
 
 using namespace std;
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
 
 #include <iostream>
 using namespace std;
